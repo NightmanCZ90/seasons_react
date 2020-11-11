f516fa97b0b8ab37ac0c6e70206521eac196a507
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-11 17:43:43 +0100

    added simple geolocation api and error handler

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      lat: null,
+      long: null,
+      errorMessage: ''
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        })
+      },
+      (err) => { this.setState({ errorMessage: err.message }) }
+    );
+  }
+  
+  render() {
     return (
-      <div>Latitude: </div>
+      this.state.errorMessage
+      ? ( <div>Error: {this.state.errorMessage}</div> )
+      : ( <div>
+            Latitude: {this.state.lat}
+            <br/>
+            Longitude: {this.state.long}
+          </div> 
+        )
     )
   }
 }
