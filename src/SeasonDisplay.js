9376ac1d5351a7517013ae4712c8a4b5c42bbcdc
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-11 18:59:45 +0100

    refactored ternary operator logic for seasons into seasonConfig object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,20 +1,33 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly!',
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter'
   } else {
     return lat > 0 ? 'winter' : 'summer'
   }
-}
+};
 
 const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth())
-  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
   
   return (
     <div>
-      {text}
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
